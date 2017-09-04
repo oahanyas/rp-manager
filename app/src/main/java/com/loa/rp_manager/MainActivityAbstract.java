92acec14c6291e9d60339e94359719e9fd1dac1d
hanyas <oahanyas>
2017-09-04 16:21:23 +0200

    [UPDATE][PLAYER] update menu

--- app/src/main/java/com/loa/rp_manager/MainActivityAbstract.java
+++ app/src/main/java/com/loa/rp_manager/MainActivityAbstract.java
@@ -1,6 +1,7 @@
 package com.loa.rp_manager;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,10 +11,14 @@ import android.view.MenuItem;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_main)
 public abstract class MainActivityAbstract extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    @ViewById(R.id.fragment_manager)
+    protected Fragment fragment;
 
     @AfterViews
     protected void afterViews(){
@@ -57,30 +62,5 @@ public abstract class MainActivityAbstract extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/player/java/com/loa/rp_manager/MainActivity.java
+++ app/src/player/java/com/loa/rp_manager/MainActivity.java
@@ -1,8 +1,38 @@
 package com.loa.rp_manager;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends MainActivityAbstract {
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        Fragment newFragment = null;
+        if (id == R.id.menu_stats) {
+        } else if (id == R.id.menu_equipment) {
+        } else if (id == R.id.menu_inventory) {
+        } else if (id == R.id.menu_battle) {
+        }
+
+        ft.replace(R.id.fragment_manager, newFragment, newFragment.getClass().getName());
+        ft.commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
