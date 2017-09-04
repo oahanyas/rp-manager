97f4c256633dec3c70ce91bddae0694e36e95280
hanyas <oahanyas>
2017-09-04 16:42:10 +0200

    [UPDATE][PLAYER] update stats fragment

--- app/src/player/java/com/loa/rp_manager/fragment/StatsPlayer.java
+++ app/src/player/java/com/loa/rp_manager/fragment/StatsPlayer.java
@@ -7,7 +7,7 @@ import com.loa.rp_manager.R;
 import org.androidannotations.annotations.EFragment;
 
 /**
- * Created by anthony.succu on 04/09/2017.
+ * Created by Hanyas on 04/09/2017.
  */
 @EFragment(R.layout.fragment_stats)
 public class StatsPlayer extends Fragment {
