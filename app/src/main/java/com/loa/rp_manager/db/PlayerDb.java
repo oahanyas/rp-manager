5755e9ed38a72157adbc1e722cfee4e9a8bc2d57
hanyas <oahanyas>
2017-09-06 11:21:45 +0200

    [UPDATE][PLAYER] add new player

--- app/src/main/java/com/loa/rp_manager/db/PlayerDb.java
+++ app/src/main/java/com/loa/rp_manager/db/PlayerDb.java
@@ -1,7 +1,12 @@
 package com.loa.rp_manager.db;
 
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
+import com.loa.rp_manager.utils.Utils;
+
+import java.sql.SQLException;
 
 /**
  * Created by Hanyas on 06/09/2017.
@@ -28,4 +33,77 @@ public class PlayerDb {
     public static final String SEXE = "SEXE";
     @DatabaseField(columnName = SEXE)
     protected Boolean sexe;
+
+    public static final String SIZE = "SIZE";
+    @DatabaseField(columnName = SIZE)
+    protected Integer size;
+
+    public static final String WEIGHT = "WEIGHT";
+    @DatabaseField(columnName = WEIGHT)
+    protected Integer weight;
+
+    public void save() throws SQLException {
+        OrmLiteSqliteOpenHelper helper = Utils.getHelper();
+
+        PlayerHelper absHelper = (PlayerHelper) helper;
+        @SuppressWarnings("unchecked")
+        Dao<PlayerDb, String> dao = absHelper.getDao(getClass());
+        dao.createOrUpdate(this);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public Boolean getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(Boolean sexe) {
+        this.sexe = sexe;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
 }
--- app/src/player/java/com/loa/rp_manager/fragment/InfoPlayer.java
+++ app/src/player/java/com/loa/rp_manager/fragment/InfoPlayer.java
@@ -3,7 +3,10 @@ package com.loa.rp_manager.fragment;
 import android.support.v4.app.Fragment;
 
 import com.loa.rp_manager.R;
+import com.loa.rp_manager.db.PlayerDb;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
 /**
@@ -11,4 +14,19 @@ import org.androidannotations.annotations.EFragment;
  */
 @EFragment(R.layout.fragment_player_information)
 public class InfoPlayer extends Fragment {
+
+    @AfterViews
+    protected void afterView(){
+
+    }
+
+    @Click(R.id.fragment_player_information_valider)
+    protected void valider(){
+        PlayerDb playerDb = new PlayerDb();
+
+        playerDb.setName();
+        playerDb.setOrigin();
+        playerDb.setRace();
+        playerDb.setSexe();
+    }
 }
